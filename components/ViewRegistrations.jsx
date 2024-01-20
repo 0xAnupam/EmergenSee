1cbdb3af7bdc1a4756aba097377d6db5714fbf41
Sanghita1 <sanghitagharami2000@mal.com>
2024-01-20 15:07:21 +0530

    view-registration page added

---
+++ components/ViewRegistrations.jsx
@@ -0,0 +1,42 @@
+function Registrations(props){
+    return(
+    <div id="registrationsCard">
+        <div id="registCompo">
+            <div id="registCompoHeader">Name :</div>
+            <div id="registCompoValue">{props.name}</div>
+        </div>
+        <div id="registCompo">
+            <div id="registCompoHeader">Location :</div>
+            <div id="registCompoValue">{props.location}</div>
+        </div>
+        <div id="registCompo">
+            <div id="registCompoHeader">Need Oxygen Cylinder :</div>
+            <div id="registCompoValue">{props.oxygen}</div>
+        </div>
+        <div id="registCompo">
+            <div id="registCompoHeader">Emergency Type :</div>
+            <div id="registCompoValue">{props.emergency}</div>
+        </div>
+        <div id="registCompo">
+            <button id="proceed">Proceed</button>
+        </div>
+        
+    </div>
+    );
+}
+export default function ViewRegistrations(){
+    return(
+        <div id="viewRegistrationsPage">
+            <div id="header">
+                Registrations
+            </div>
+            <div id="container">
+                <Registrations name="arindam" location="beleghata" oxygen="Yes" emergency="car accident" />
+                <Registrations name="arindam" location="beleghata" oxygen="Yes" emergency="car accident" />
+                <Registrations name="arindam" location="beleghata" oxygen="Yes" emergency="car accident" />
+                <Registrations name="arindam" location="beleghata" oxygen="Yes" emergency="car accident" />
+            </div>
+        </div>
+    );
+
+}
