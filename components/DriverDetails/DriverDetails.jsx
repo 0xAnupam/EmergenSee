36a0e9eb594b9ac2123d9836c9c48df69bf0fec7
Tanmay Roy <tanmayroy84368@gmail.com>
2024-01-20 17:04:57 +0530

    user request and driver matching

--- components/DriverDetails/DriverDetails.jsx
+++ components/DriverDetails/DriverDetails.jsx
@@ -1,4 +1,24 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function Details(props) {
+  const reg_id = props.reg_id;
+  console.log(reg_id);
+
+  function handleClick() {
+    const data = {
+      hospital_name: props.hospital_name,
+      vehicleNo: props.vehicleNo,
+      driver_phone: props.contactNo,
+    };
+    console.log(reg_id);
+    axios.patch(`/api/ambulances/${reg_id}`, {
+      hospital_name: data.hospital_name,
+      vehicle_no: data.vehicleNo,
+      driver_phone: data.driver_phone,
+    });
+    props.onDriverData(data);
+  }
   return (
     <div id="registrationsCard">
       <div id="registCompo">
@@ -15,7 +35,7 @@ function Details(props) {
         </div> */}
       <div id="registCompo">
         <div id="registCompoHeader">Has Oxygen Cylinder :</div>
-        <div id="registCompoValue">{props.oxygen}</div>
+        <div id="registCompoValue">{props.oxygen ? "Yes" : "No"}</div>
       </div>
       <div id="registCompo">
         <div id="registCompoHeader">Vehicle No :</div>
@@ -23,34 +43,58 @@ function Details(props) {
       </div>
       <div id="registCompo">
         <div id="registCompoHeader">Contact No :</div>
-        <div id="registCompoValue">{props.driver_phone}</div>
+        <div id="registCompoValue">{props.contactNo}</div>
       </div>
       <div id="registCompo">
-        <button id="proceed">Proceed</button>
+        <button onClick={handleClick} id="proceed">
+          Assign
+        </button>
       </div>
     </div>
   );
 }
 
-export default function DriverDetials() {
+export default function DriverDetials(props) {
+  const [driverDetails, setDriverDetails] = useState([]);
+  const [driverData, setDriverData] = useState({});
+
+  const reg_id = props.reg_id;
+  //   console.log(reg_id);
+
+  props.onDriverData(driverData);
+
+  useEffect(() => {
+    axios.get("/api/drivers").then((res) => {
+      const data = res.data;
+      const filteredData = data.filter((d) => {
+        return d.available === true;
+      });
+      setDriverDetails(filteredData);
+    });
+  }, []);
+
   return (
     <div id="viewRegistrationsPage">
       <div id="header">Available Drivers</div>
       <div id="container">
-        <Details
-          name="Manoj"
-          email="manoj@gmail.com"
-          oxygen="Yes"
-          vehicleNo="WB4567"
-          contactNo="8536974589"
-        />
-        <Details
-          name="Dinesh"
-          email="dinesh@gmail.com"
-          oxygen="Yes"
-          vehicleNo="WB6543"
-          contactNo="9875624894"
-        />
+        {driverDetails &&
+          driverDetails.map((data, id) => {
+            return (
+              <Details
+                key={id}
+                name={data.name}
+                email={data.email}
+                oxygen={data.oxygenSupport}
+                vehicleNo={data.vehicleNo}
+                contactNo="8965324795"
+                hospital_name={data.hospitalName}
+                reg_id={reg_id}
+                onDriverData={(data) => {
+                  setDriverData(data);
+                }}
+              />
+            );
+          })}
       </div>
     </div>
   );
--- components/ViewRegistrations.jsx
+++ components/ViewRegistrations.jsx
@@ -1,44 +1,128 @@
-function Registrations(props){
-    const reg_id = props.id;
+import { useEffect, useState } from "react";
+import Modal from "react-modal";
+import DriverDetials from "./DriverDetails/DriverDetails";
+import axios from "axios";
 
-    return(
+function Registrations(props) {
+  const reg_id = props.id;
+  //   console.log(reg_id);
+
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+    },
+  };
+
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [driverData, setDriverData] = useState(null);
+
+  //   useEffect(() => {
+  //     if (driverData) {
+  //       axios.patch(`/api/ambulances/${reg_id}`, {
+  //         hospital_name: driverData.hospital_name,
+  //         vehicle_no: driverData.vehicleNo,
+  //         driver_phone: driverData.driver_phone,
+  //       });
+  //     }
+  //   }, [driverData]);
+
+  //   console.log(driverData);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  return (
     <div id="registrationsCard">
-        <div id="registCompo">
-            <div id="registCompoHeader">Name :</div>
-            <div id="registCompoValue">{props.name}</div>
-        </div>
-        <div id="registCompo">
-            <div id="registCompoHeader">Location :</div>
-            <div id="registCompoValue">{props.location}</div>
-        </div>
-        <div id="registCompo">
-            <div id="registCompoHeader">Need Oxygen Cylinder :</div>
-            <div id="registCompoValue">{props.oxygen}</div>
-        </div>
-        <div id="registCompo">
-            <div id="registCompoHeader">Emergency Type :</div>
-            <div id="registCompoValue">{props.emergency}</div>
-        </div>
-        <div id="registCompo">
-            <button id="proceed">Proceed</button>
-        </div>
-        
+      <div id="registCompo">
+        <div id="registCompoHeader">Name :</div>
+        <div id="registCompoValue">{props.name}</div>
+      </div>
+      <div id="registCompo">
+        <div id="registCompoHeader">Location :</div>
+        <div id="registCompoValue">{props.location}</div>
+      </div>
+      <div id="registCompo">
+        <div id="registCompoHeader">Need Oxygen Cylinder :</div>
+        <div id="registCompoValue">{props.oxygen}</div>
+      </div>
+      <div id="registCompo">
+        <div id="registCompoHeader">Emergency Type :</div>
+        <div id="registCompoValue">{props.emergency}</div>
+      </div>
+      <div id="registCompo">
+        <button id="proceed" onClick={openModal}>
+          Proceed
+        </button>
+      </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <DriverDetials
+          reg_id={reg_id}
+          onDriverData={(data) => {
+            setDriverData(data);
+          }}
+        />
+      </Modal>
     </div>
-    );
+  );
 }
-export default function ViewRegistrations(){
-    return(
-        <div id="viewRegistrationsPage">
-            <div id="header">
-                Registrations
-            </div>
-            <div id="container">
-                <Registrations name="arindam" location="beleghata" oxygen="Yes" emergency="car accident" />
-                <Registrations name="arindam" location="beleghata" oxygen="Yes" emergency="car accident" />
-                <Registrations name="arindam" location="beleghata" oxygen="Yes" emergency="car accident" />
-                <Registrations name="arindam" location="beleghata" oxygen="Yes" emergency="car accident" />
-            </div>
-        </div>
-    );
+export default function ViewRegistrations() {
+  const [requests, setRequests] = useState([]);
 
+  useEffect(() => {
+    axios.get("/api/ambulances").then((res) => {
+      console.log(res.data);
+      setRequests(res.data);
+    });
+  }, []);
+
+  return (
+    <div id="viewRegistrationsPage">
+      <div id="header">Registrations</div>
+      <div id="container">
+        {/* <Registrations
+          name="arindam"
+          location="beleghata"
+          oxygen="Yes"
+          emergency="car accident"
+          onDriverData={(data) => {
+            setDriverData(data);
+          }}
+        /> */}
+
+        {requests &&
+          requests.map((data, idx) => {
+            console.log(data);
+
+            return (
+              <Registrations
+                key={idx}
+                id={data._id}
+                name={data.name}
+                location={data.location}
+                oxygen={data.oxygenSupport}
+                emergency={data.emergencyType}
+                onDriverData={(data) => {
+                  setDriverData(data);
+                }}
+              />
+            );
+          })}
+      </div>
+    </div>
+  );
 }
