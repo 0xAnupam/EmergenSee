18f5a7c6b7c673a8fc23ae921a74061fb244477a
Sanghita1 <sanghitagharami2000@mal.com>
2024-01-20 12:56:52 +0530

    notification page added

---
+++ components/Notification.jsx
@@ -0,0 +1,15 @@
+export default function Notification() {
+    return (
+        <div id="notifPage">
+        <div id="notifHeader">
+            Notification
+        </div>
+        <div id="notifContent">
+            <ul>
+                <li>Hello</li>
+                <li>gggggggggggggg ggggggggggg ggggggggggg</li>
+            </ul>
+        </div>
+        </div>
+    );
+}
